008c726261fa4f97738b98720db833016fe4d15e
Viktor <al.viktor@gmail.com>
2019-06-10 16:09:03 +0300

    Eleventh lesson

---
+++ String_values.rb
@@ -0,0 +1,7 @@
+no_of_apples = 5
+student = "John"
+price = 6.99
+
+puts student + ' has '  + no_of_apples.to_s + ' apples. It cost him $' + price.to_s
+puts "______________________________________________________"
+puts "#{price} has #{no_of_apples} apples. It cost him #{price}"
---
+++ ex03.rb
@@ -0,0 +1,23 @@
+puts "I will be counting my chickens soon:"
+
+puts "Hens #{25.0 + 30.0 / 6.0}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+puts "Next I will count the eggs:"
+
+puts 3 + 2 + 1 -5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's falce!"
+
+puts "How about some more?"
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal #{5 <= -2}"
